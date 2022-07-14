d9307a1808e5126172b9d497660043d35ac1ff5b
Pedro Conrado <conradohpedro@gmail.com>
2022-07-13 23:36:55 -0300

    create Comment component

---
+++ src/components/Comment/Comment.jsx
@@ -0,0 +1,33 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/PedroHConrado.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Pedro Conrado</strong>
+              <time title='12 de Julho ás 17:16' dateTime='2022-07-12 17:16:30'>Cerca de 1h atrás</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24}/>
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
