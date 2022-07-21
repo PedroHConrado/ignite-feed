58f82654ff16863c5508047b71e48f95c70ac41f
Pedro Conrado <conradohpedro@gmail.com>
2022-07-20 21:31:01 -0300

    create posts

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,37 @@ import styles from './App.module.css'
 import './global.css';
 
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/PedroHConrado.png',
+      name: 'Pedro Conrado',
+      role: 'Web developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no emu portifa. É um'},
+      { type: 'link', content: 'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2022-07-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    }, 
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no emu portifa. É um'},
+      { type: 'link', content: 'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2022-07-08 20:00:00'),
+  },
+]
+
 function App() {
   return (
     <>
@@ -15,8 +46,15 @@ function App() {
     <div className={styles.wrapper}>
       <Sidebar />
       <main>
-        <Post />
-        <Post />
+        {posts.map(post => {
+          return (
+            <Post 
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          )
+        })}
       </main>
     </div>
     </>
